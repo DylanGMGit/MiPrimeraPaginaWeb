41c848fc50553687d943f441d6d2bc97691c2bbf
DylanGMGit <116947272+DylanGMGit@users.noreply.github.com>
2022-12-28 18:17:38 +0100

    28/12/22
    1) Se ha añadido diseño a los botones "Send Message" y "Follow Me"
    2) Se han añadido las funciones a los botones "Send Message" y "Follow Me"

---
+++ logicas/logicTarjetaContacto.js
@@ -0,0 +1,7 @@
+function mandarCorreo() {
+    window.location.href = "mailto:dylan022010@gmail.com";
+}
+
+function seguirInstagram() {
+    window.open("https://www.instagram.com/dylang_esp/","_blank");
+}
